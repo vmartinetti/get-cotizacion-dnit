79e2339881cbbabc84177221fc9bb1aa2d7a9c6c
MranDvX <franmavazq@gmail.com>
2025-03-30 21:59:51 -0300

    feat: add Spanish month constants and timeout configurations

---
+++ src/constants.ts
@@ -0,0 +1,10 @@
+export const MONTHS_IN_SPANISH = [
+  "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", 
+  "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"
+];
+
+export const DNIT_URL = "https://www.dnit.gov.py/en/web/portal-institucional/cotizaciones";
+export const DAY_INDEX = 0;
+export const COMPRA_INDEX = 1;
+export const VENTA_INDEX = 2;
+export const TIMEOUT_MS = 10000; // 10 seconds 
